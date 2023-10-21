e132fb6d4f6a7b236f6a23d75565a3edf6b44b8e
Gorgees Odisho <gorgees041@gmail.com>
2023-10-21 05:20:58 +0300

    STATE & EVENTS ASSIGNMENT DONE

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addCard, setAddCard] = useState(false);
   return (
-    <li className="">
+    <li className={addCard ? "in-cart" : null}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setAddCard(!addCard)}>
+        {addCard ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
